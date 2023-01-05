8cf26e529d094d59b6b76e0a79b20bd20979b93e
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-01-05 16:56:03 +0000

    add ls_plot_group_facet initial build

---
+++ R/ls_plot_group_facet.R
@@ -0,0 +1,36 @@
+ls_plot_group_facet <- function(df, x_var = V1, y_var = V2, group_var, nrow){
+
+  group_sym <- rlang::ensym(group_var)
+
+  group_names <- df %>% dplyr::pull({{group_var}}) %>% unique()
+
+  facet_plots <- purrr::map(
+    group_names,
+    ~ df %>%
+      dplyr::slice_sample(n = nrow(df)) %>%
+      dplyr::mutate(plotting_var = as.character({{group_var}}),
+                    highlight = ifelse(plotting_var == .x, 'A', 'B')) %>%
+      ggplot2::ggplot(aes(x = {{x_var}}, y = {{y_var}}, colour = highlight, alpha = highlight)) +
+      ggplot2::geom_point(shape = ".") +
+      ggplot2::theme_bw() +
+      ggplot2::scale_colour_manual(values = c(
+        "A" = "blue",
+        "B" = "grey80")) +
+      ggplot2::scale_alpha_manual(values = c("A" = 1,
+                                    "B" = 0.5)) +
+      ggplot2::theme(
+        legend.position = "none",
+        panel.grid = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank()) +
+      ggplot2::labs(title = paste0(.x),
+           x = "",
+           y = "")
+  )
+
+  wrapped_plots <- patchwork::wrap_plots(facet_plots, nrow = nrow)
+
+  return(wrapped_plots)
+
+}
+
