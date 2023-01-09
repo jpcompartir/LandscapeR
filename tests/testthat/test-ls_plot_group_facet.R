2d41d3906ea712d083ca7a1f7715678588877bb9
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-01-09 17:11:54 +0000

    add initial tests for ls_plot_group_facet

---
+++ tests/testthat/test-ls_plot_group_facet.R
@@ -0,0 +1,20 @@
+test_that("ls_plot_group_facet is working as intended", {
+  df <- ls_example
+
+  #Check badly input arguments raise error
+  expect_error(ls_plot_group_facet(df, x = v1))
+  expect_error(ls_plot_group_facet(df, y = "blank"))
+
+  #Check plot doesn't render if group_var isn't set
+  expect_error(ls_plot_group_facet(df, nrow = 3))
+
+  #outputs are returned properly
+  plot <- ls_plot_group_facet(ls_example, group_var = cluster)
+  expect_true("patchwork" %in% class(plot))
+  plot <- ls_plot_group_facet(ls_example, group_var = cluster, output = "list_of_plots")
+  expect_equal(length(plot), 7)
+  expect_type(plot, "list")
+  plot <- ls_plot_group_facet(ls_example, group_var = cluster, output = "wrapped_and_list")
+  expect_true("wrapped" %in% names(plot) & "list_plots" %in% names(plot))
+
+})
